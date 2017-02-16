c74aea562ecc73a6d29b72db17d6f39f177fc8c7
Bhatti <hummylin@gmail.com>
2017-02-16 22:20:33 +0500

    added footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Telecom :) === (: UET Taxila</title>
+        <title>Telecom | UET Taxila</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -82,15 +82,19 @@
                     Telecom Department
                 </div>
                 </br>
-                <p> Excellent Forum to Express Suggestions!</p>
+                <p> Forum to Express Your Suggestions!</p>
 
                 <div class="links">
-                    <a href="https://bbc.com">Link</a>
+                    <a href="https://uettaxila.edu.pk">Link</a>
 {{--                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
  --}}           </div>
+            </div>
+
+            <div>
+            <p class="text-center">Copyright Mustafa Bhatti - All Rights Reserved</p>
             </div>
         </div>
     </body>
